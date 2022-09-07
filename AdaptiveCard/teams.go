66e951e1a381a745ac522bc8726416c8d6867497
Stefan Mantel <stefan.mantel@4data.ch>
2022-09-07 14:17:27 +0200

    added http.Client

--- AdaptiveCard/teams.go
+++ AdaptiveCard/teams.go
@@ -2,27 +2,54 @@ package teams
 
 import (
 	"bytes"
-	"crypto/tls"
 	"encoding/json"
-	"log"
+	"errors"
+	"fmt"
 	"net/http"
 )
 
-type Client struct {
-	Webhook string
-}
-
+//
 type Webhook struct {
+	// Webhook URL where the message is sent to
 	Url string
+	// http client used to send the request including whatever configuration required
+	client *http.Client
 }
 
-func NewWebhook(url string) *Webhook {
-	return &Webhook{
-		Url: url,
+func NewWebhook(url string) (*Webhook, error) {
+	webhook := &Webhook{}
+
+	if ok := isValidUri(url); ok != true {
+		return nil, errors.New("url is not valid")
+	} else {
+		webhook.Url = url
 	}
+
+	client := &http.Client{}
+
+	webhook.client = client
+
+	return webhook, nil
 }
 
-func (w *Webhook) Send(msg *Message) error {
+func (w *Webhook) Send(card Card) error {
+	var msg interface{}
+	switch card.(type) {
+	case *AdaptiveCard:
+		msg = &Message{
+			Type: "message",
+			Attachments: []Attachment{
+				{
+					ContentType: "application/vnd.microsoft.card.adaptive",
+					ContentUrl:  "",
+					Content:     card,
+				},
+			},
+		}
+	default:
+		msg = card
+	}
+
 	json, err := json.Marshal(msg)
 	if err != nil {
 		return err
@@ -35,19 +62,13 @@ func (w *Webhook) Send(msg *Message) error {
 
 	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
 
-	client := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true,
-			},
-		},
-	}
-
-	res, err := client.Do(req)
+	res, err := w.client.Do(req)
 	if err != nil {
 		return err
 	} else {
-		log.Printf("%+v", res)
+		if res.StatusCode != http.StatusOK {
+			return errors.New(fmt.Sprintf("Expected StatusCode:%d, but got %d", http.StatusOK, res.StatusCode))
+		}
 	}
 
 	return nil
